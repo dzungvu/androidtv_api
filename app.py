f8061ab197697b5d712942725fb539cb107ba274
DzungVu <dzungvucs@gmail.com>
2021-05-23 23:20:09 +0700

    Test authorization

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Header
+from fastapi.security import OAuth2PasswordBearer
+
 from pydantic import BaseModel
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 class Item(BaseModel):
     name: str
@@ -15,10 +20,14 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: Optional[str] = None, token: str = Depends(oauth2_scheme)):
     return {"item_id": item_id, "q": q}
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+def update_item(item_id: int, item: Item, token: str = Depends(oauth2_scheme)):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
